752ea6d67edfd4865178307f6ed70efecaee5047
Artur Ishmuratov <ru_man1987@icloud.com>
2024-01-24 14:37:10 +0300

    start backend develop(express, nodemon jwt), tested with insomnia, made simple requests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello world 111")
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: 'Марат Сафин',
+        },
+        'secret123',
+    );
+    res.json({
+        success: true,
+        token,
+    });
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Server OK');
+})
