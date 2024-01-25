0e6cd3414a532789e8fe1b64c82570bc5177f9d5
Artur Ishmuratov <ru_man1987@icloud.com>
2024-01-25 17:16:08 +0300

    bag fixed

--- index.js
+++ index.js
@@ -23,49 +23,50 @@ const app = express();
 
 app.use(express.json());
 
-try {
-    app.post('/auth/register', registerValidation, async (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
-    
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-    
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        });
-    
-        const user = await doc.save();
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array())
+    }
 
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            },
-        );
-    
-            const { passwordHash, ...userData} = user._doc
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-        res.json({
-            ...userData,
-            token,
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+        {
+            _id: user._id,
+        },
+        'secret123',
+        {
+            expiresIn: '30d',
+        },
+    );
+
+        const { passwordHash, ...userData} = user._doc
+
+    res.json({
+        ...userData,
+        token,
+    });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегистрировать пользователя'
         });
-    });
-} catch(err) {
-    console.log(err);
-    res.status(500).json({
-        message: 'Не удалось зарегистрировать пользователя'
-    });
-}
+    }
+});
+
 
 app.listen(4444, (err) => {
     if (err) {
