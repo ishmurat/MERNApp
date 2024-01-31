bfdfdc76c862c87b4c9c598c22a7112e8aaefb02
Artur Ishmuratov <ru_man1987@icloud.com>
2024-01-31 15:53:38 +0300

    All bugs fixed 1.16

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { validationResult } from "express-validator";
 import { registerValidation } from './validations/auth.js';
 
 import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
 
 import bcrypt from 'bcrypt'
 
@@ -106,6 +107,25 @@ app.post('/auth/login', async (req, res) => {
     }
 })
 
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+        if (!user) {
+            return res.status(404).json({
+                message:'Пользователь не найден'
+            })
+        }
+        const { passwordHash, ...userData} = user._doc
+
+        res.json(userData);
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Нет доступа'
+        });
+    }
+})
 
 app.listen(4444, (err) => {
     if (err) {
