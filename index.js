cecb3709b6c79a9c2d1c96e1acfbbc4240e32639
Artur Ishmuratov <ru_man1987@icloud.com>
2024-02-01 16:11:57 +0300

    Post adding added

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation } from './validations/auth.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 // import { register, login, getMe } from './controllers/UserController.js'
 import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
 
 mongoose
 .connect('mongodb+srv://admin:wwwwww@cluster0.9fnxnck.mongodb.net/blog?retryWrites=true&w=majority')
@@ -18,10 +19,14 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, UserController.register);
+app.post('/auth/login', loginValidation, UserController.login);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post('/auth/login', UserController.login)
-
-app.get('/auth/me', checkAuth, UserController.getMe)
+// app.get('/posts', PostController.getAll);
+// app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+// app.get('/posts', PostController.remove);
+// app.get('/posts', PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {
